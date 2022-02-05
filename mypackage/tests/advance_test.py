bce6071a53b0d1a10c712a91f639c35c85c598a4
Phanindra Thorati <thoratiphanindra@gmail.com>
2022-02-05 16:31:48 +0530

    Added project to github

---
+++ mypackage/tests/advance_test.py
@@ -0,0 +1,16 @@
+from stringsfuns import palindrome , pangram , anagram , alphaorder , isalphabets
+
+s='abba'
+s1='abba'
+
+a=palindrome(s)
+b=pangram(s)
+c=anagram(s,s1)
+d=alphaorder(s)
+e=isalphabets(s)
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
---
+++ mypackage/tests/basic_test.py
@@ -0,0 +1,22 @@
+from stringsfuns import length , uppercase , title , comparison , reverse , replace , spliting_lines
+
+s='string.methods'
+s1='string.methods'
+s2='s'
+s3='g'
+
+a=length(s)
+b=uppercase(s)
+c=title(s)
+d=comparison(s,s1)
+e=reverse(s)
+f=replace(s,s2,s3)
+g=spliting_lines(s)
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
